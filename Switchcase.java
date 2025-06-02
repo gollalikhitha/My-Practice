09dc2ac38bbb5470822d958e89f16ac4de1fd796
gollalikhitha <likhithalikky77@gmail.com>
2025-06-02 22:47:38 +0530

    Add files via upload

---
+++ Switchcase.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+public class Switchcase
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    switch(n)
+	    {
+	        case 1:System.out.println("mon");
+	        break;
+	        case 2:System.out.println("tus");
+	        break;
+	        case 3:System.out.println("wed");
+	        break;
+	        case 4:System.out.println("thu");
+	        break;
+	        case 5:System.out.println("frid");
+	        break;
+	        case 6:System.out.println("sat");
+	        break;
+	        case 7:System.out.println("sun");
+	        break;
+	        default:System.out.println("invalid");
+	   
+	    }
+	            
+	    
+	}
+}
