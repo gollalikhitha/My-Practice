5f6f27170c7541c3a493634e4e700edabbed9293
gollalikhitha <likhithalikky77@gmail.com>
2025-06-03 14:01:18 +0530

    Add files via upload

---
+++ Twostringseq.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Twostringseq
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String a=sc.nextLine();
+	    String b=sc.nextLine();
+	    if(a.equals(b)){
+	    
+	     
+		System.out.println(true);
+	    }
+		else{
+		    System.out.println(false);
+		
+	    }
+		
+	}
+}
