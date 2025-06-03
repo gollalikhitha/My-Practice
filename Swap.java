420064fe10c1073d836af2cf52efb6c8db779a11
gollalikhitha <likhithalikky77@gmail.com>
2025-06-03 14:00:35 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,15 @@
+
+
+public class Swap
+{
+	public static void main(String[] args) {
+	    int a=3,b=4;
+	     int temp=0;
+	     temp=a;
+	    a=b;
+	    b=temp;
+	    System.out.println(a+" "+b);
+	    
+		
+	}
+}
